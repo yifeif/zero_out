4a09c1bcdd4caaef7aa4074f8b3b0b6ec2d8c5bd
Yifei Feng <yifeif@google.com>
2018-10-24 17:30:06 -0700

    clean up build file

--- tensorflow_zero_out/python/ops/zero_out_ops.py
+++ tensorflow_zero_out/python/ops/zero_out_ops.py
@@ -1,7 +1,6 @@
-from tensorflow.python.framework import load_library
-from tensorflow.python.framework import ops
+from tensorflow.contrib.util import loader
 from tensorflow.python.platform import resource_loader
 
-_zero_out_ops = load_library.load_op_library(
+_zero_out_ops = loader.load_op_library(
     resource_loader.get_path_to_datafile('_zero_out_ops.so'))
 zero_out = _zero_out_ops.zero_out
